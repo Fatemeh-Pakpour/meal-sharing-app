1fbdf2f30552caab012742bcffc8cdaafb4ba260
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-10-21 12:01:03 +0200

    added the bootstrap and css style

--- src/frontend/pages/home.js
+++ src/frontend/pages/home.js
@@ -8,13 +8,23 @@ function homeRouter(req, router) {
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav ml-4">
-                <a class="nav-item nav-link" href="http://localhost:3000">HOME <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="http://localhost:3000/meals">MEALS</a>
-                <a class="nav-item nav-link" href="http://localhost:3000/reviews">REVIEWS</a>
+                <a class="nav-item nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">MEALS</a>
+                <a class="nav-item nav-link" href="#">REVIEWS</a>
             </div>
         </div>
     </div>
   </nav>
+  
+  <div class="col-lg order-lg-2 text-center">
+    <h3 class = "my-4">Let's Eat</h3>
+    <img  class = "mb-4 img-fluid rounded center-block" src="../assets/mealsharing.jpg" alt="meal sharing">
+    <h4>Discover Home Cooking in 150+ Counties!</h4>
+  </div>
+  <div class ="container">
+  <h1 class="display-4 text-center my-5 text-muted">Featured Meals</h1>
+    <div id="featured-meal-container"></div>
+  </div>
   `;
 }
 
