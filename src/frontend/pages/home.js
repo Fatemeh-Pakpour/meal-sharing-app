8e2cc15b8633bd205a05577aee8599ae81876281
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-10-21 11:51:29 +0200

    added the navigation bar to home.js

--- src/frontend/pages/home.js
+++ src/frontend/pages/home.js
@@ -1,5 +1,21 @@
 function homeRouter(req, router) {
-  document.body.innerHTML = "<h1>Home</h1>";
+  document.body.innerHTML = `
+  <nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top">
+    <div class ="container">
+        <a class="navbar-brand order-1 mr-0">MEAL SHARING </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-4">
+                <a class="nav-item nav-link" href="http://localhost:3000">HOME <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="http://localhost:3000/meals">MEALS</a>
+                <a class="nav-item nav-link" href="http://localhost:3000/reviews">REVIEWS</a>
+            </div>
+        </div>
+    </div>
+  </nav>
+  `;
 }
 
 export default homeRouter;
